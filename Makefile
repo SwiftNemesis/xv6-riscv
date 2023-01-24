e7c4d2743e5c52aedb8ab83ad8e1b0f858274d7a
SwiftNemesis <bbsowers@gmail.com>
2023-01-23 22:40:19 -0800

    Updated stat, makefile

--- Makefile
+++ Makefile
@@ -133,6 +133,9 @@ UPROGS=\
 	$U/_wc\
 	$U/_zombie\
 	$U/_hello\
+	$U/_stat\
+
+# Updated to have stat as a user program.
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
--- user/stat.c
+++ user/stat.c
@@ -3,13 +3,36 @@
 #include "user/user.h"
 
 
+//enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
+
 int main(int argc, char *argv[])
 {
-    printf("Process PID:\t " + getpid());
-    printf("Memory Used:\t " + getmem());
-    printf("Proc State:\t " + getstate());
-    printf("Uptime:\t " + uptime());
-    printf("Parnet PID:\t " + getparentpid());
-    //printf("Page Table Address:\t " + )
-
+    printf("Process PID:\t%d\n", getpid());
+    printf("Memory Used:\t%d\n", getmem());
+    int temp = getstate();
+    switch(temp)
+    {
+        case 0:
+            printf("Proc State:\tUNUSED\n");
+            break;
+        case 1:
+            printf("Proc State:\tUSED\n");
+            break;
+        case 2:
+            printf("Proc State:\tSLEEPING\n");
+            break;
+        case 3:
+            printf("Proc State:\tRUNNABLE\n");
+            break;
+        case 4:
+            printf("Proc State:\tRUNNING\n");
+            break;
+        case 5:
+            printf("Proc State:\tZOMBIE\n");
+            break;
+    }
+    printf("Uptime (ticks):\t%d\n", uptime());
+    printf("Parnet PID:\t%d\n", getparentpid());
+    printf("Page Tble Addr:\t%x\n", getkstack());
+    return 0;
 }
